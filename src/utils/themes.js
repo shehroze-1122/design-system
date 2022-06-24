b332652bf0f990eff220893c6a8267255ade5d54
shehroze-1122 <stalat.bee19seecs@seecs.edu.pk>
2022-06-24 13:08:55 +0500

    design system utilities

---
+++ src/utils/themes.js
@@ -0,0 +1,13 @@
+import { blue, neutral, text } from "./colors";
+import { primaryFont } from "./typography";
+
+export const defaultTheme = {
+    primaryColor: blue[300],
+    primaryHoverColor: blue[200],
+    primaryActiveColor: blue[100],
+    primaryDisabledColor: neutral[400],
+    textColorOnPrimary: text.simple,
+    textColor: text.simple,
+    textColorInverted: text.inverted,
+    primaryFont
+}
